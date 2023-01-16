a3ea2ec0c0b7ab21892c11a7f1f018aa2155f84e
ViktoriiaVerych <113999567+ViktoriiaVerych@users.noreply.github.com>
2023-01-16 07:29:40 +0200

    Add files via upload

--- arcanoid.py
+++ arcanoid.py
@@ -25,17 +25,18 @@ class Ball:
         self.color = (200, 0, 0)
 
         # self.rect = pygame.Rect(self.x, self.y, 6, 6)
-        self.actor = Actor('r_b.png')
+        self.actor = Actor('r_b.png',(x, y))
     def draw(self):
         self.actor.draw()
+
 
 
 class Brick:
     def __init__(self, x, y, randomcolor):
         self.x = x
         self.y = y
-        # self.color = randomcolor
-        # self.rect = pygame.Rect(self.x, self.y, 50, 20)
+        self.color = randomcolor
+        self.rect = pygame.Rect(self.x, self.y, 50, 20)
 
     def update(self):
         pygame.draw.rect(screen, self.color, (self.x, self.y, 50, 20))
@@ -54,23 +55,23 @@ class Paddle:
 
 
 paddle = Paddle(350, 370)
-ball = Ball(200, 200)
+# ball = Ball(200, 200)
 
 # paddle = pygame.image.load('p_p.jpg')
 # paddle = Actor('p_p.jpg')
 # paddle.x = 350
 # paddle.y = 370
 
-ball = Ball(0,0)
+ball = Ball(250,280)
 ball_x_speed = -3
-ball_y_speed = -3
+ball_y_speed = 3
 
 bars_list = []
 
 def draw():
     surface = pygame.display.set_mode((WIDTH, HEIGHT))
 
-
+    screen.clear()
     color = (0, 100, 100)
     surface.fill(color)
     pygame.display.flip()
@@ -94,37 +95,43 @@ def on_mouse_move(pos):
     x = pos[0]
     paddle.rect.centerx = x
 
-# def update(dt):
-#     global ball_x_speed, ball_y_speed
-#     # if keyboard.left:
-#     #     paddle.x = paddle.x - 5
-#     # if keyboard.right:
-#     #     paddle.x = paddle.x + 5
-#
-#
-#     update_ball()
-#     for bar in bars_list:
-#         if ball.colliderect(bar):
-#             bars_list.remove(bar)
-#             ball_y_speed *= -1
-#
-#     if paddle.colliderect(ball):
-#         ball_y_speed *= -1
-#         rand = random.randint(0,1)
-#         if rand:
-#             ball_x_speed *= -1
-#
-# def update_ball():
-#     global ball_x_speed, ball_y_speed
-#     ball.x -= ball_x_speed
-#     ball.y -= ball_y_speed
-#     if (ball.x >= WIDTH) or (ball.x <=0):
-#         ball_x_speed *= -1
-#     if (ball.y >= HEIGHT) or (ball.y <=0):
-#         ball_y_speed *= -1
-#     if ball.y > HEIGHT:
-#         pass
+def update(dt):
+    global ball_x_speed, ball_y_speed
+    # if keyboard.left:
+    #     paddle.x = paddle.x - 5
+    # if keyboard.right:
+    #     paddle.x = paddle.x + 5
+
+
+    update_ball()
+    for bar in bars_list:
+        if ball.actor.colliderect(bar):
+            bars_list.remove(bar)
+            ball_y_speed *= -1
+
+
+        # rand = random.randint(0,1)
+        # if rand:
+        #     ball_x_speed *= -1
+
+def update_ball():
+    global ball_x_speed, ball_y_speed
+
+    if (ball.actor.x >= WIDTH) or (ball.actor.x <=0):
+        ball_x_speed *= -1
+    if (ball.actor.y <=0):
+        ball_y_speed *= -1
+
+    if ball.actor.y >= paddle.rect.y:
+        if paddle.rect.bottomleft[0] <= ball.actor.x <= paddle.rect.bottomright[0]:
+            ball_y_speed *= -1
+        else:
+            pass
         #lives counter func
+
+    ball.actor.x -= ball_x_speed
+    ball.actor.y += ball_y_speed
+        
 def lives():
     pass
 
